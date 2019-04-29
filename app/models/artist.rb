c8c4b63576a1a2617b7313e83083f6f8c3a4ba9b
github username <github email address>
2019-04-29 12:56:21 -0400

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,11 @@
 class Artist < ActiveRecord::Base
+  has_many :songs
+
+  def song_count
+    self.songs.length
+  end
+
+  def format_song_count
+    "#{self.song_count} #{'song'.pluralize(self.song_count)}"
+  end
 end
